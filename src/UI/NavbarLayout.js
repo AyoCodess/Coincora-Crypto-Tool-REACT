6a671312580436539e25b19293c60ae5547ce455
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-12-16 17:00:57 +0100

    netlify build fix

--- src/UI/NavbarLayout.js
+++ src/UI/NavbarLayout.js
@@ -1,13 +1,11 @@
 import './NavbarLayout.scss';
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
