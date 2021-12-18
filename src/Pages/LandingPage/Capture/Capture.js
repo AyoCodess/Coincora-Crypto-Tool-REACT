bee3df692dc458a410c05f90f3afdf795d661792
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-12-18 17:47:55 +0100

    added captcha

--- src/Pages/LandingPage/Capture/Capture.js
+++ src/Pages/LandingPage/Capture/Capture.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 
 function Capture() {
-  //   function onChange(value) {
-  //     console.log('Captcha value:', value);
-  //   }
+  function onChange(value) {
+    console.log('Captcha value:', value);
+  }
 
   return (
     <>
-      {/* <div>hey</div> */}
-      {/* <ReCAPTCHA sitekey={process.env.GOOGLE_FORM} onChange={onChange} /> */}
+      <div>CAPTURE FORM</div>
+      <ReCAPTCHA
+        sitekey={process.env.REACT_APP_GOOGLE_FORM}
+        onChange={onChange}
+      />
     </>
   );
 }
