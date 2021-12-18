0cacaadb3643248f5fc4bba1979358a80124f473
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-12-18 18:56:29 +0100

    capta v2 implemented

--- src/Pages/LandingPage/GoogleForm/GoogleForm.js
+++ src/Pages/LandingPage/GoogleForm/GoogleForm.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import './GoogleForm.scss';
 import FingerprintIcon from '@mui/icons-material/Fingerprint';
-import Capture from '../Capture/Capture';
 
 function GoogleForm() {
-  const recaptchaRef = React.createRef();
   return (
     <>
       <div
         className='flex flex-col justify-center items-center'
         style={{ height: '85vh' }}>
         <form
-          onSubmit={(e) => {
-            console.log({ e });
-            recaptchaRef.current.execute();
-            console.log('sent');
-            fetch(
-              'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfOljMM0BkFLBFikUm8X1Sdq3-TwJpyTtZbzmpDc1TMoo1FSQ/formResponse',
-              {
-                method: 'POST',
-              }
-            );
-          }}
-          //   action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSfOljMM0BkFLBFikUm8X1Sdq3-TwJpyTtZbzmpDc1TMoo1FSQ/formResponse'
-        >
+          action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSfOljMM0BkFLBFikUm8X1Sdq3-TwJpyTtZbzmpDc1TMoo1FSQ/formResponse'
+          method='POST'>
           <div className='border-2 lg:pt-5 lg:w-96 rounded-full boxShadow border-2 border-gray-200'>
             <div className='flex flex-col justify-center items-center gap-5 p-10'>
               <label className='lg:text-5xl text-3xl text-center w-60 mt-5  hover:text-blue-500 text-gray-400 transition ease-in-out duration-700 '>
@@ -47,7 +34,17 @@ function GoogleForm() {
               </button>
             </div>
           </div>
-          <Capture />
+          <div className='flex justify-center items-center gap-2 mt-5'>
+            <labelc className=''>reCAPTCHA</labelc>
+            <span> 2 + 2 </span>
+            <input
+              type='number'
+              required
+              name='entry.1369813225'
+              className='w-12 h-15 text-lg rounded-full textFieldGoogleForm placeholder p-2 ml-2 text-center focus:outline-sky-300 border-appBlue border-2'
+              placeholder='Join the waiting list'
+            />
+          </div>
         </form>
         <p className='w-full text-center text-3xl mt-10 text-gray-300 hover:text-gray-500 transition ease-in-out duration-700'>
           Predict. Profit. Conquer
