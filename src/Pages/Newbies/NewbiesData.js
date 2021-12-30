8bfe4c5b15d49fcf0478a9ac9d289c56156c863f
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-12-30 18:08:01 +0700

    moved item data to its own file

---
+++ src/Pages/Newbies/NewbiesData.js
@@ -0,0 +1,111 @@
+const itemData = [
+  {
+    img: 'https://i.imgur.com/5gnYPMh.png',
+    title: 'Binance Exchange',
+    info: 'Offering a wide range crypto assets.',
+    moreInfo:
+      'Binance features great and many emerging crypto projects. If you have some experience, you need this exchange.',
+    alt: 'binance logo',
+    link: 'https://accounts.binance.com/en/register?ref=37749695',
+    status: 'Must have',
+  },
+  {
+    img: 'https://i.imgur.com/wnhi5Ho.png',
+    title: 'Coinbase Exchange',
+    info: 'Making buying crypto super simple.',
+    moreInfo:
+      'if you want to invest in the safer more established coins, easily via simple app experience, Coinbase is the way to go.',
+    alt: 'coinbase logo',
+    link: 'https://www.coinbase.com/join/adesan_c9?src=ios-link',
+    status: 'Must have',
+  },
+  {
+    img: 'https://i.imgur.com/YUegSx8.png',
+    title: 'Coingecko',
+    info: 'Track your crypto portfolio comprehensively.',
+    moreInfo:
+      'This is the best investment portfolio tracker available right now.',
+    alt: 'coingecko logo',
+    link: 'https://www.coingecko.com/',
+    status: 'Must have',
+  },
+  {
+    img: 'https://i.imgur.com/uhdaSOe.png',
+    title: 'CoinMarketCap',
+    info: 'Get free crypto and track your portfolio.',
+    moreInfo:
+      "CoinMarketCap is owned and ran by Binance. If you subscribe to their platform you can get notified on 'air drops' (free crypto giveaways). You can also track your coins here similar to coingecko but not as comprehensive.",
+    alt: 'coinmarketcap logo',
+    link: 'https://coinmarketcap.com/invite?ref=94PLEUUN',
+    status: 'Must have',
+  },
+  {
+    img: 'https://i.imgur.com/ppQvdwK.png',
+    title: 'Celsius',
+    info: 'Earn up to 17% a year on all your crypto.',
+    moreInfo:
+      'We use and recommend Celsius to earn compounding interest on your crypto investment portfolio.',
+    alt: 'Celsius logo',
+    link: 'https://celsiusnetwork.app.link/111453048f',
+    status: 'Optional',
+  },
+  {
+    img: 'https://i.imgur.com/TrnVE8N.png',
+    title: 'Kucoin Exchange',
+    info: 'Find the most new and promising crypto projects',
+    moreInfo:
+      'Kucoin is infamous for launching coins that go on to do great things',
+    alt: 'kucoin',
+    link: 'https://www.kucoin.plus/land/register/r/22raNy7',
+    status: 'Must have',
+  },
+  {
+    img: 'https://i.imgur.com/6qokTZk.png',
+    title: 'Token Sniffer',
+    info: 'Quickly detect scam coins',
+    moreInfo:
+      'This tool helps you detect scam coins by simply supplying the coin address to auto-generate a useful report about the coin integrity.',
+    alt: 'token sniffer',
+    link: 'https://tokensniffer.com/',
+    status: 'Optional',
+  },
+  {
+    img: 'https://i.imgur.com/fiIXkNx.png',
+    title: 'Trust Wallet',
+    info: 'Store all your crypto in one place',
+    moreInfo:
+      'An off exchange multi-wallet. You have total control over your crypto.',
+    alt: 'trust wallet logo',
+    link: 'hhttps://trustwallet.com/',
+    status: 'Optional',
+  },
+  {
+    img: 'https://i.imgur.com/5Bz2aih.png',
+    title: 'Wise',
+    info: 'Crypto friendly international bank account',
+    moreInfo: 'Our recommend bank account to use for crypto transactions',
+    alt: 'wise logo',
+    link: 'https://wise.com/invite/iwh/ayo-olaa',
+    status: 'Optional',
+  },
+  {
+    img: 'https://i.imgur.com/TPPxPX2.png',
+    title: 'Revolut',
+    info: 'Crypto friendly international bank account',
+    moreInfo: 'Use for crypto transactions',
+    alt: 'Revolut logo',
+    link: 'https://revolut.com/referral/ayoi8wid!DEC1AR',
+    status: 'Optional',
+  },
+  {
+    img: 'https://i.imgur.com/2p0quQ5.png',
+    title: 'Crypto Panic',
+    info: 'Curated crypto news',
+    moreInfo: 'Our recommend and central hub for breaking crypto news',
+    alt: 'Crypto Panic logo',
+    link: 'https://cryptopanic.com/',
+    status: 'Optional',
+  },
+];
+
+export default itemData;
