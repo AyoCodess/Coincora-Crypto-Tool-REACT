41fbc55a0ef9821b6230cf2d013a4cfb3f3f2f94
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-03-26 00:32:12 +0800

    coin predict form updated - button update

--- src/components/Buttons/StandardButtonTypeButton.js
+++ src/components/Buttons/StandardButtonTypeButton.js
@@ -13,7 +13,7 @@ function StandardButtonTypeButton({
       style={{ cursor: 'pointer' }}
       type='button'
       onClick={onClick}
-      className={` ${textColor} justify-center py-2 px-4 border border-transparent shadow-sm text-lg text-white font-medium rounded-md bg-appBlue  ${color} ${width}  ${textColorHover} `}>
+      className={` ${textColor} justify-center py-2 px-4 border border-transparent shadow-sm text-lg text-white font-medium rounded-md bg-appBlue  hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-500  ${color} ${width}  ${textColorHover} `}>
       {text}
     </button>
   );
