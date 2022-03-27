4559d1452f25387cdab635f9b0e394cecdb3065a
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-03-27 19:05:44 +0800

    added active state to newbies page buttons

--- src/components/Buttons/SortButton.js
+++ src/components/Buttons/SortButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function SortButton({ onClick, text, setMustHave, setOptional }) {
+function SortButton({ onClick, text, setMustHave, setOptional, active }) {
   return (
     <button
-      style={{ cursor: 'pointer' }}
+      style={{
+        backgroundColor: active ? '#0554FE' : '',
+        color: active ? 'white' : ' ',
+        cursor: 'pointer',
+      }}
       onClick={onClick}
       type='button'
       className='text-sm md:text-lg p-1 md:p-2 rounded-md border-2 border-gray-300 shadow-md hover:bg-appBlue hover:text-white transition duration-500 ease-in-out'>
